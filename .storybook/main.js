8ffa939f13cdc5d95b126c98de41af85aa644b7b
Luke Policinski <luke.policinski@egov.com>
2020-03-27 11:42:50 -0400

    custom-babel-rc

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,6 @@
+require('core-js');
+require('regenerator-runtime/runtime');
+
 module.exports = {
   stories: ['../stories/**/*.stories.js'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links'],
