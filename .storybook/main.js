e1f2647b58d1197a7e65765e518d95d67afaa576
Luke Policinski <luke.policinski@egov.com>
2020-03-30 11:48:52 -0400

    temp

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,3 @@
-require('core-js');
-require('regenerator-runtime/runtime');
-
 module.exports = {
   stories: ['../stories/**/*.stories.js'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links'],
